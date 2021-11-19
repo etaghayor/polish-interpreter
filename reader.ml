34117be20407b0105e50320e2b1a63c959e773af
Ellenor <f.taghayor@gmail.com>
2021-11-19 15:55:17 +0100

    simple read_operation

--- reader.ml
+++ reader.ml
@@ -2,10 +2,15 @@ open Ast
 open String
 open Str
 
-let read_op = function
-  | _ -> failwith "TODO"
 
-let read_expr line = failwith "TODO"
+let contains_sub s1 s2 =
+  let re = Str.regexp_string s2
+  in
+  try ignore (Str.search_forward re s1 0); true
+  with Not_found -> false
+
+let read_var_name name = 
+  if List.length name = 1 then List.hd name else failwith "syntax error: not a valid name"
 
 
 let check_indent depth pos line =
@@ -15,7 +20,6 @@ let check_indent depth pos line =
     | n , [] -> (depth-n,pos,line)
   in aux depth line
 
-let check_name name = List.length name = 1 
 
 let read_op op =
   match op with 
@@ -24,37 +28,55 @@ let read_op op =
   | "*" -> Mul
   | "/" -> Div
   | "%" -> Mod
-  | _ -> Not_op
+  | _ -> failwith "Not_op"
 
- 
-let read_expression expr res=
-  let splitted = split_on_char " " expr in
-  let res = None in
-  match splitted with
-  |None -> res
-  |x::y -> if (read_op x) <> Not_op then 
-    match int_of_string x
-    | n -> Num n
-    | exception _ -> Var x
-else
-  Op read_op x match y with
-  | [] | [x] -> failwith "NOOOOO"
-      | x1::x2::xs -> read_expression x1 
 
-  (read_expression [List.hd y]) (read_expression (List.tl y))
+let read_expr expr =
+  let splitted = split_on_char ' ' expr in
+  let rec aux = function
+    | [] -> failwith "not a valid excpression"
+    | x::y -> match read_op x with
+      | exception _ -> (match x with
+          | n -> Num (int_of_string n)
+          | exception _ -> Var x)
+      | _ -> Op (read_op x,aux y,aux y)
+  in aux splitted
+
+let read_comp = function
+  | " = " -> Eq
+  | " <> " -> Ne
+  | " < " -> Lt
+  | " <= " -> Le
+  | " > " -> Gt
+  | " >= " -> Ge
+  | _ -> failwith "NOT A VALID OPERATOR"
+
+let aux_read_cond e d = 
+  let elist = split (regexp_string d) e in
+  read_expr (List.hd elist), d, read_expr (List.nth elist 1)
+
+let read_cond e = 
+  let e1,c,e2 = aux_read_cond e
+      (if contains_sub e " = " then " = "  else 
+       if contains_sub e " <> " then " <> " else
+       if contains_sub e " < " then " < " else
+       if contains_sub e " <= " then " <= " else
+       if contains_sub e " > " then " > " else
+       if contains_sub e " => " then " => " else
+         failwith "NOT A VALID CONDITION" )
+  in (e1,read_comp c, e2) 
 
 let read_instr lb pos line =
   let depth,pos,line = check_indent lb pos line in
   if depth > lb then failwith ("indentation bug: line " ^ (string_of_int pos)) else
     match List.hd line with
-    | "READ" -> let name = List.tl line in 
-      if check_name name then (depth,Read (List.hd name)) else failwith "NOT INSTRUCTION"
-    | "PRINT" -> let expr = List.tl line in
-     depth, Print(List.hd expr)
+    | "READ" -> let name = List.tl line in (depth,Read (read_var_name name))
+    | "PRINT" -> failwith "TODO"
     | "IF" -> failwith "TODO"
     | "WHILE" -> failwith "TODO"
     | "COMMENT" -> failwith "TODO"
     | _ -> failwith "TODO SET"
+
 
 
 let read_block b = failwith "TODO"
@@ -67,10 +89,11 @@ let read_lines (filename:string) =
     | None -> res
   in loop 1 []
 
+
 let read_polish (filename:string) (*: program*) = 
   let lines = read_lines filename in
   let rec aux last_b res = function
     | (p,x)::xs -> let depth,instr = read_instr last_b p (split_on_char ' ' x) in 
-      aux lb ((p,instr)::res) xs
+      aux depth ((p,instr)::res) xs
     | [] -> res
   in aux 0 [] lines
